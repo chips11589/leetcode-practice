91ac8fed2d8a0f0069cc08647c1e8e7c013cad14
Tri Vien <tri.pham.ngoc.vien@steadfasttech.com.au>
2021-07-05 22:08:51 +0100

    Add project files.

---
+++ ChipsPlayGround/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ChipsPlayGround
+{
+    public class Solution
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            const int MAX_VALUE = 10001;
+            int buyPrice = MAX_VALUE;
+            int profit = 0;
+            int sellPrice = 0;
+            int current = MAX_VALUE;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (i == prices.Length - 1 && sellPrice <= prices[i])
+                {
+                    current = MAX_VALUE;
+                    sellPrice = prices[i];
+                }
+
+                if (current > prices[i])
+                {
+                    if (sellPrice > buyPrice)
+                    {
+                        profit += (sellPrice - buyPrice);
+                        sellPrice = 0;
+                    }
+                    buyPrice = prices[i];
+                }
+                else if (buyPrice < prices[i])
+                {
+                    sellPrice = prices[i];
+                }
+                current = prices[i];
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ ChipsTest/SolutionTest.cs
@@ -0,0 +1,22 @@
+using ChipsPlayGround;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChipsTest
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        [DataRow(new int[] { 7, 1, 5, 3, 6, 4 }, 7)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 4)]
+        [DataRow(new int[] { 7, 6, 4, 3, 1 }, 0)]
+        [DataRow(new int[] { 6, 1, 3, 2, 4, 7 }, 7)]
+        [DataRow(new int[] { 2, 4, 1 }, 2)]
+        [DataRow(new int[] { 2, 1, 2, 0, 1 }, 2)]
+        [DataRow(new int[] { 1, 9, 6, 9, 1, 7, 1, 1, 5, 9, 9, 9 }, 25)]
+        public void MaxProfit(int[] prices, int profit)
+        {
+            Assert.AreEqual(profit, Solution.MaxProfit(prices));
+        }
+    }
+}
