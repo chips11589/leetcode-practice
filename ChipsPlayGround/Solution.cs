859ad39df85bc7c00028285dd051989ac27920de
Tristan <gachip11589@gmail.com>
2025-01-06 21:20:54 +0000

    IsSameTree

--- ChipsPlayGround/Solution.cs
+++ ChipsPlayGround/Solution.cs
@@ -4444,4 +4444,18 @@ public class Solution
 
         return maxDepth;
     }
+
+    /// <summary>
+    /// https://leetcode.com/problems/same-tree
+    /// </summary>
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p?.val != q?.val)
+            return false;
+
+        if (p == null && q == null)
+            return true;
+
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
 }
